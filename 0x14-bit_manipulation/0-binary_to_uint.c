0b048903b59b70bc4e76950efd622162a9e98fd3
rahma2005 <rhmarhma360@gmail.com>
2023-06-09 13:25:35 -0700

    movrp

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 	j = 0;
 	if (!b)
 		return (0);
-	for (i = 0; b[i] != NULL; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
